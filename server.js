b5bf8cf4a8e6336706bcd5775b40c0039589da7d
Yener Yardim <yyardim@gmail.com>
2017-08-10 23:16:30 -0700

    generic routing logic added

--- server.js
+++ server.js
@@ -1,41 +1,85 @@
 // Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
+const express       = require('express'),
+      exphbs        = require('express-handlebars'),
+      hbsHelpers    = require('handlebars-helpers'),
+      hbsLayouts    = require('handlebars-layouts'),
+      path          = require('path'),
+      bodyParser    = require('body-parser'),
+      session       = require('express-session'),
+      csrf          = require('csurf'),
+      errorhandler  = require('errorhandler'),
+      morgan        = require('morgan'),
+      favicon       = require('serve-favicon'),
+      
+    // database     = require('./lib/database'),
+    // seeder       = require('./lib/dbSeeder'),
+      router        = require('./server/routes/router'),
+      app           = express(),
+      port          = '3000';
 
-// Get our API routes
-const api = require('./server/routes/api');
+class Server {
 
-const app = express();
+  constructor()  {
+    this.initViewEngine();
+    this.initExpressMiddleware();
+    this.initCustomMiddlware();
+    this.initDbSeeder();
+    this.initRoutes();
+    this.start();
+  }
 
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+  start() {
+    app.listen(port, (err) => {
+      console.log(`API running on http://localhost:${port}`);
+    })
+  }
 
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+  initViewEngine() {
+    const hbs = exphbs.create({
+      extname: '.hbs',
+      defaultLayout: 'master'
+    });
+    app.engine('hbs', hbs.engine);
+    app.set('view engine', 'hbs');
+    hbsLayouts.register(hbs.handlebars, {});
+  }
 
-// Set our api routes
-app.use('/api', api);
+  initExpressMiddleware() {
+    app.use(favicon(__dirname + '/dist/favicon.ico'));
+    app.use(express.static(path.join(__dirname, 'dist')));
+    app.use(morgan('dev'));
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(session({
+      secret: 'zigzock05',
+      saveUninitialized: true,
+      resave: true })
+    );
+    app.use(errorhandler());
+    app.use(csrf());
 
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+    app.use(function (req, res, next) {
+      res.locals._csrf = req.csrfToken();
+      next();
+    });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
+    process.on('uncaughtException', function (err) {
+      if (err) console.log(err, err.stack);
+    });
+  }
 
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
+  initCustomMiddlware() {}
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+  initDbSeeder() {}
+
+  initRoutes() {
+    //router.load(app, './server/controllers');
+
+    // redirect all others to the index (HTML5 history)
+    app.all('/*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'dist/index.html'));
+    })
+  }
+}
+
+var server = new Server();
